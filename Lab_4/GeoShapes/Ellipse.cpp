c9d2a0d5a5b125117436c3e18e9817d52135546e
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-03-21 11:41:57 +0300

    Class ellipse was added

---
+++ Lab_4/GeoShapes/Ellipse.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Алексей Николаев on 21.03.2023.
+//
+
+#include "Ellipse.h"
---
+++ Lab_4/GeoShapes/Ellipse.h
@@ -0,0 +1,16 @@
+//
+// Created by Алексей Николаев on 21.03.2023.
+//
+
+#ifndef LAB_4_ELLIPSE_H
+#define LAB_4_ELLIPSE_H
+
+
+
+class Ellipse {
+
+};
+
+
+
+#endif //LAB_4_ELLIPSE_H
