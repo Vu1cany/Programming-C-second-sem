65437a6fa5bfa58b08cf80a7c41c3178ce0baddb
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2023-04-27 17:15:48 +0300

    MyQueue was created

---
+++ Lab_5/MyQueue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Алексей Николаев on 27.04.2023.
+//
+
+#include "MyQueue.h"
---
+++ Lab_5/MyQueue.h
@@ -0,0 +1,12 @@
+//
+// Created by Алексей Николаев on 27.04.2023.
+//
+
+#ifndef LAB_5_MYQUEUE_H
+#define LAB_5_MYQUEUE_H
+
+class MyQueue {
+    
+};
+
+#endif //LAB_5_MYQUEUE_H
